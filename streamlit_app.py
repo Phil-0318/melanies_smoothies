3a5be6f6599ef83907be1fb789fa666fb94dddc5
Phil-0318 <phil.lavin@rojihealthintel.com>
2026-07-06 15:27:43 -0400

    Refactor smoothie data fetching and display logic

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,12 +31,10 @@ ingredients_list = st.multiselect(
     max_selections=5
 )
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-
 if ingredients_list:
     ingredients_string = ''
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon") 
+    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
